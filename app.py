d4f55c77be2984d80e37ccfcddd9e6aa60f3052e
pranavghimire05-cpu <pranavghimire05@gmail.com>
2026-04-30 17:42:37 +0545

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model = joblib.load("Logistic.pkl")
 scaler = joblib.load("scaler.pkl")
 expected_columns = joblib.load("columns.pkl")
 
-st.title("Heart Stroke Prediction by akarsh")
+st.title("Heart Stroke Prediction")
 st.markdown("Provide the following details to check your heart stroke risk:")
 
 # Collect user input
